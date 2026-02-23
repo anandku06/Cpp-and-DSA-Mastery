c5dc92afa173c35dc5c552787796a91d06fc7241
Anand Kumar <rishavgupta163@gmail.com>
2026-02-23 12:46:08 +0530

    Add implementation for "Binary Gap" problem with examples and explanation

---
+++ POTD/leetcode/countGap.cpp
@@ -0,0 +1,61 @@
+// 868. Binary Gap
+
+// Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+// Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+
+ 
+
+// Example 1:
+
+// Input: n = 22
+// Output: 2
+// Explanation: 22 in binary is "10110".
+// The first adjacent pair of 1's is "10110" with a distance of 2.
+// The second adjacent pair of 1's is "10110" with a distance of 1.
+// The answer is the largest of these two distances, which is 2.
+// Note that "10110" is not a valid pair since there is a 1 separating the two 1's underlined.
+// Example 2:
+
+// Input: n = 8
+// Output: 0
+// Explanation: 8 in binary is "1000".
+// There are not any adjacent pairs of 1's in the binary representation of 8, so we return 0.
+// Example 3:
+
+// Input: n = 5
+// Output: 2
+// Explanation: 5 in binary is "101".
+ 
+
+// Constraints:
+
+// 1 <= n <= 109
+
+// intuition:
+// The idea is to iterate through the bits of the given integer n and keep track of the positions of the 1's. We can use a variable to store the position of the previous 1 and another variable to store the current position. Whenever we encounter a 1, we can calculate the distance between the current position and the previous position and update the result if it is greater than the current result.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int countGaps(int n) {
+            int curr = 0;
+            int prev = -1;
+
+            int result = 0;
+
+            while(n > 0) {
+                if((n & 1) > 0) {
+                    result = (prev != -1) ? max(result, curr - prev) : result;
+                    prev = curr;
+                }
+
+                curr++;
+                n >>= 1;
+            }
+
+            return result;
+        }
+};
