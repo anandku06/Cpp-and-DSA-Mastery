6d9f33d456d03577c8e69562f944cddd7ae5b651
Anand Kumar <rishavgupta163@gmail.com>
2026-07-07 15:21:46 +0530

    Refactor inOrder and preOrder traversal implementations for consistency and clarity

--- DSA/Tree/Binary Tree/inOrder.cpp
+++ DSA/Tree/Binary Tree/inOrder.cpp
@@ -4,38 +4,48 @@ using namespace std;
 struct TreeNode
 {
     int data;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 
     TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
 };
 
+void inOrderRecursive(TreeNode *root, vector<int> &result)
+{
+    if (root == nullptr)
+        return;
 
-vector<int> inOrderTraversal(TreeNode* root)
+    inOrderRecursive(root->left, result);
+    result.push_back(root->data);
+    inOrderRecursive(root->right, result);
+}
+
+vector<int> inOrderTraversal(TreeNode *root)
 {
     vector<int> inorder;
-    if(root == nullptr) return inorder;
+    if (root == nullptr)
+        return inorder;
 
-    stack<TreeNode*> st;
-    TreeNode* elem = root;
+    stack<TreeNode *> st;
+    TreeNode *elem = root;
 
     while (true)
     {
-        if(elem != nullptr)
+        if (elem != nullptr)
         {
             st.push(elem);
             elem = elem->left;
         }
         else
         {
-            if(st.empty()) break;
+            if (st.empty())
+                break;
             elem = st.top();
             st.pop();
             inorder.push_back(elem->data);
             elem = elem->right;
         }
-        
     }
-    
+
     return inorder;
 }
--- DSA/Tree/Binary Tree/preOrder.cpp
+++ DSA/Tree/Binary Tree/preOrder.cpp
@@ -4,34 +4,44 @@ using namespace std;
 struct TreeNode
 {
     int data;
-    TreeNode* left;
-    TreeNode* right;
+    TreeNode *left;
+    TreeNode *right;
 
-    TreeNode(int c) : data(c), left(nullptr), right(nullptr){}
+    TreeNode(int c) : data(c), left(nullptr), right(nullptr) {}
 };
 
-vector<int> preOrderIterative(TreeNode* root)
+void preOrderRecursive(TreeNode *root, vector<int> &result)
+{
+    if (root == nullptr)
+        return;
+
+    result.push_back(root->data);
+    preOrderRecursive(root->left, result);
+    preOrderRecursive(root->right, result);
+}
+
+vector<int> preOrderIterative(TreeNode *root)
 {
     vector<int> result;
 
-    if(root == nullptr) return result;
+    if (root == nullptr)
+        return result;
 
-    stack<TreeNode*> st;
+    stack<TreeNode *> st;
     st.push(root);
 
-    while (!st.empty())     
+    while (!st.empty())
     {
         root = st.top();
         st.pop();
         result.push_back(root->data);
 
-        if(root->right != nullptr)
-        st.push(root->right);
+        if (root->right != nullptr)
+            st.push(root->right);
 
-        if(root->left != nullptr)
-        st.push(root->left);
+        if (root->left != nullptr)
+            st.push(root->left);
     }
 
     return result;
-    
 }
